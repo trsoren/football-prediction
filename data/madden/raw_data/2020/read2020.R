e02facf46139b00c9a77875c62272d8ab0167255
gmsasina <gmsasina@umich.edu>
2023-02-10 17:43:01 -0500

    finished

---
+++ data/madden/raw_data/2020/read2020.R
@@ -0,0 +1,17 @@
+library(readxl)
+
+
+master_ratings = data.frame(matrix(ncol=4, nrow=0))
+colnames(master_ratings) = c("year", "team", "position", "overall")
+setwd("/Users/grantsasina/desktop/eecs_448/football-prediction/data/madden/raw_data/2020/raw")
+for (fn in list.files()) {
+  
+  team = read_excel(fn)
+  name = strsplit(fn, "__madden") [[1]][1]
+  
+  df = data.frame(year=2020, team=name, position = team$Position, overall=team$Overall)
+  
+  master_ratings=rbind(master_ratings, df)
+}
+
+write.csv(master_ratings, "data2020.csv", row.names=FALSE)
